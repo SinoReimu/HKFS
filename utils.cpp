067a66aeeb4b7276a024d2ea4bbae7306cf8ba80
sinoreimu <su@geekzh.com>
2017-05-31 06:45:54 +0800

    fix split

--- utils.cpp
+++ utils.cpp
@@ -4,14 +4,19 @@
 using namespace std;
 
 void split(string& s, const string& delim, vector<string>* ret) {
-	size_t last = 0;
+	size_t  last = 0;
 	size_t index=s.find_first_of(delim,last);
+	int size = s.length();
+	if(index==0) {
+		index=s.find_first_of(delim, 1);
+		last+=1;	
+	}
 	while (index!=string::npos)
 	{
 		ret->push_back(s.substr(last,index-last));
 		last=index+1;
 		index=s.find_first_of(delim,last);
 	}
-	if (index-last>0)
-		ret->push_back(s.substr(last,index-last));
+	if (size-last>0)
+		ret->push_back(s.substr(last,size-last));
 }
